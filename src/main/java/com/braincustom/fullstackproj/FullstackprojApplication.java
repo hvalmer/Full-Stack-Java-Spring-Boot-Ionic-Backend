ddbe70c0b844a9d2d2a363b2e23491299713b796
hvalmer <hvalmer@gmail.com>
2020-06-20 16:42:03 -0300

    Projeto foi criado!

---
+++ src/main/java/com/braincustom/fullstackproj/FullstackprojApplication.java
@@ -0,0 +1,13 @@
+package com.braincustom.fullstackproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braincustom/fullstackproj/FullstackprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincustom.fullstackproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
