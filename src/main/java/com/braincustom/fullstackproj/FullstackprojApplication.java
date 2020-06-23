62e7c2101e27de4cf9de6bc9f69d1addd3c0b722
hvalmer <hvalmer@gmail.com>
2020-06-23 12:00:45 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/braincustom/fullstackproj/FullstackprojApplication.java
+++ src/main/java/com/braincustom/fullstackproj/FullstackprojApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.braincustom.fullstackproj.domain.Categoria;
+import com.braincustom.fullstackproj.domain.Produto;
 import com.braincustom.fullstackproj.repositories.CategoriaRepository;
+import com.braincustom.fullstackproj.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class FullstackprojApplication implements CommandLineRunner {
@@ -17,19 +19,37 @@ public class FullstackprojApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	//dependência do objeto Produto
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackprojApplication.class, args);
 	}
 
-	//instanciando os objetos da Categoria
+	//instanciando os objetos da Categoria e do Produto
 	@Override
 	public void run(String... args) throws Exception {
 			
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		//associando as categorias com os produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		//associando os produtos as sua categoria
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		//objeto responsável por salvar as categorias no BD
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
