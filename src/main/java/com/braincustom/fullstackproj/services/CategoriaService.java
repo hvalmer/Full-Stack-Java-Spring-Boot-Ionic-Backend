2207c49a842cbf07a161122a903bcfdf7e8af0bb
hvalmer <hvalmer@gmail.com>
2020-07-11 17:46:54 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/braincustom/fullstackproj/services/CategoriaService.java
+++ src/main/java/com/braincustom/fullstackproj/services/CategoriaService.java
@@ -20,4 +20,10 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 		}
+	
+	//método INSERT
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return reposi.save(obj);
+	}
 }
